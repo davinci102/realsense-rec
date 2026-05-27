9bd319da1e02e69a4988f10493c6e6a3c00fd020
davinci102 <denis.w@mail.ru>
2026-05-27 12:10:34 +0500

    новая версия

--- realsense-rec.py
+++ realsense-rec.py
@@ -3,6 +3,7 @@
 RealSense D415 Dataset Recorder — GUI Mini-App (v4 FIXED)
 - Мониторинг CPU, RAM, скорости записи на диск, свободного места
 - Исправлена ошибка с psutil.disk_io_counters (namedtuple immutability)
+- тест
 """
 import tkinter as tk
 from tkinter import ttk, messagebox
